bab27f53073a6593b05ac429a8051727187cdfa5
Suhani Zaman <suhanibadiz9@gmail.com>
2021-08-09 18:59:12 +0400

    added routes sinatra_returning_html.md

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
-
+require "sinatra/reloader" if development?
 get '/' do
   "hello!"
+end
+
+get '/secret' do
+  "how are you?"
+end
+get '/public' do
+  "HOW DARE YOU?"
+end
+
+get '/cat' do
+  "<div style='border: dashed red'>
+
+  <img src='http://i.imgur.com/jFaSxym.png'>
+  
+  </div>"
 end
